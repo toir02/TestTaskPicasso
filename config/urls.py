92b320d8544c87545df046069bf61b34607e132f
toir02 <wend8765@gmail.com>
2023-11-01 22:56:02 +0300

    added documentation for api with redoc

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,29 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="Документация для API по обработке и загрузке файлов",
+        terms_of_service="https://www.example.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny, ],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include('files.urls', namespace='files')),
     path('users/', include('users.urls', namespace='users')),
-] + + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
