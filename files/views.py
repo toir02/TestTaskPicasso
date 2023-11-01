2851d13e85f51318c9b24d0ab1c89ce0359ad2cb
toir02 <wend8765@gmail.com>
2023-11-01 23:30:51 +0300

    added file create api view

--- files/views.py
+++ files/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from files.models import File
+from files.serializers import FileSerializer
+from files.tasks import process_file
+
+
+class FileCreateAPIView(generics.CreateAPIView):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        process_file.delay(instance.id)
+        serializer = FileSerializer(instance)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
