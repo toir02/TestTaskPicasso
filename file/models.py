4ebe920b09bf770a1f617bc43ee72fddcfc841bb
toir02 <wend8765@gmail.com>
2023-11-01 22:27:36 +0300

    added file model

--- file/models.py
+++ file/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    file = models.FileField(upload_to='files/', verbose_name='файл')
+    uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name='дата и время загрузки')
+    processed = models.BooleanField(default=False, verbose_name='признак обработки')
+
+    class Meta:
+        verbose_name = 'Файл'
+        verbose_name_plural = 'Файлы'
