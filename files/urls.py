3608015e03f7fda48392ea20e4356aa9e3999b5b
toir02 <wend8765@gmail.com>
2023-11-01 23:32:23 +0300

    added path to endpoint upload

--- files/urls.py
+++ files/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path
+
 from files.apps import FileConfig
+from files.views import FileCreateAPIView
 
 app_name = FileConfig.name
+
+urlpatterns = [
+    path('upload/', FileCreateAPIView.as_view(), name='file-upload'),
+]
