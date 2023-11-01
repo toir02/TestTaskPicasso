a3138a08647aa5fc28b619061c61dd0aae2e5bc7
toir02 <wend8765@gmail.com>
2023-11-01 23:38:46 +0300

    rename uploaded to path

--- files/models.py
+++ files/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class File(models.Model):
-    file = models.FileField(upload_to='files/', verbose_name='файл')
+    file = models.FileField(upload_to='uploaded_files/', verbose_name='файл')
     uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name='дата и время загрузки')
     processed = models.BooleanField(default=False, verbose_name='признак обработки')
 
