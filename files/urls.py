69d09e208730a8a759c3227de5580a4774312ec9
toir02 <wend8765@gmail.com>
2023-11-02 12:08:04 +0300

    rename path

--- files/urls.py
+++ files/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from files.apps import FileConfig
-from files.views import FileCreateAPIView
+from files.views import FileCreateAPIView, FileListAPIView
 
 app_name = FileConfig.name
 
 urlpatterns = [
     path('upload/', FileCreateAPIView.as_view(), name='file-upload'),
-    path('upload/', FileCreateAPIView.as_view(), name='files-list'),
+    path('files/', FileListAPIView.as_view(), name='files-list'),
 ]
