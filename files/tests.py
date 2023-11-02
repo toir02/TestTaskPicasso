52ab38147b047d3129d2c99739528e201c6768ad
toir02 <wend8765@gmail.com>
2023-11-02 14:06:57 +0300

    created file test case with test get files

--- files/tests.py
+++ files/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from files.models import File
+from users.models import User
+
+
+class FileTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test user",
+            password="12345",
+        )
+        self.client.force_authenticate(user=self.user)
+
+        self.file = File.objects.create(file='1')
+
+    def test_get_files(self):
+        url = reverse('files:files-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
